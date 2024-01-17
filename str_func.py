06f3912fa7462cfef2b7fda48e7469c0b08a0cd4
bogdanovdenis95 <bogdanov-denis95@mail.ru>
2024-01-17 10:04:42 +0300

    Added str_func.py with a function

---
+++ str_func.py
@@ -0,0 +1,11 @@
+def capitalize_string(input_string):
+    """
+    Takes a string as input and returns it in all uppercase letters.
+
+    Parameters:
+    - input_string (str): The input string.
+
+    Returns:
+    - str: The input string in uppercase.
+    """
+    return input_string.upper()
